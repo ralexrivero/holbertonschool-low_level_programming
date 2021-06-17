e957a40259a6ed6e12e5c59b23d2a5eba8302d48
Ronald Rivero <ralexrivero@gmail.com>
2021-06-17 14:09:33 -0700

    Low level programing C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - Prints a specified string
+*
+ *Description: with proper grammar, but the outcome is a piece of art,
+ *Return: 0 if the programming ran to completion
+ */
+int putchar(int char)
+{
+	
+	return (0);
+}
