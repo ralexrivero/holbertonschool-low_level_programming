abd432043d107323047ef008d81a08828f050dd3
Ronald Rivero <ralexrivero@gmail.com>
2021-06-17 13:32:33 -0700

    Low level programing C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *main - using printf to display text
+  *
+  *description: diplay: with proper grammar, but the outcome is a piece of art,
+  *return: 0 when finish
+  */
+int main(void)
+{
+	printf();
+	return(0);
+}
