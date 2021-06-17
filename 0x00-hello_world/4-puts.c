9099b33b0f519b138f77dbc4e6f0db69b8777f50
Ronald Rivero <ralexrivero@gmail.com>
2021-06-17 13:07:16 -0700

    Low level programing C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 /**
 *main - "Programming is like building a multilingual puzzle"
 *testing and aplling betty style
+*return:0 when finish successfully
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +10,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-/**
-*return:0 when finish successfully
-*/
-
