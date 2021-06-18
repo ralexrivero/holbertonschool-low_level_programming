fbbc820be86280d1a9930b2917d1e412f79007bd
Ronald Rivero <ralexrivero@gmail.com>
2021-06-17 18:30:15 -0700

    low level c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,8 +5,23 @@
  *Description: with proper grammar, but the outcome is a piece of art,
  *Return: 0 if the programming ran to completion
  */
-int main(void)
+int main()
 {
-	printf("%zu\n", "Size of a char: sizeof(int)) byte\(s\)";
+	int varInt_int
+	long int varLng_lngi
+	long long int varLLI_lli
+	float varFlt_float
+	printf("%zu\n", "Size of a char: sizeof(varInt_int)) byte\(s\)";
+	printf("%zu\n", "Size of a char: sizeof(varLng_Lngi)) byte\(s\)";
+	printf("%zu\n", "Size of a char: sizeof(varLLI_lli)) byte\(s\)";
+	printf("%zu\n", "Size of a char: sizeof(varFlt_float)) byte\(s\)";
 	return (0);
 }
+
+
+/* 
+* Size of an int: 4 byte(s)
+* Size of a long int: 4 byte(s)
+* Size of a long long int: 8 byte(s)
+* Size of a float: 4 byte(s)
+*/
