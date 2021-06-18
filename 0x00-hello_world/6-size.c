f1e78849693fe69e0162c593cb9e17d4c4eabdb9
Ronald Rivero <ralexrivero@gmail.com>
2021-06-17 18:51:36 -0700

    low level c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,13 @@
  */
 int main()
 {
+	char varChr_char;
 	int varInt_int;
 	long int varLng_lngi;
 	long long int varLLI_lli;
 	float varFlt_float;
 
+	printf("Size of a char: %zu  byte (s)\n", sizeof(varChr_char);
 	printf("Size of a int: %zu  byte (s)\n", sizeof(varInt_int));
 	printf("Size of a long int: %zu  byte (s)\n", sizeof(varLng_lngi));
 	printf("Size of a log long int: %zu  byte (s)\n", sizeof(varLLI_lli));
