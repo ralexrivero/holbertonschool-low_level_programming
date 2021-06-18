78f96e3a4df6bdd2401186dbcf75a987eefacb9e
Ronald Rivero <ralexrivero@gmail.com>
2021-06-17 19:53:25 -0700

    low level c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015    -10-19",stderr);
-
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stderr);
 	return (1);
 }
