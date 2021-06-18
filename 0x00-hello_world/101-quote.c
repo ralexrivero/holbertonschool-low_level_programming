e168d57313961adec747bbbb7572bdaea24106b7
Ronald Rivero <ralexrivero@gmail.com>
2021-06-17 19:43:45 -0700

    low level c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Prints a specified string
+*
+ *Description: with proper grammar, but the outcome is a piece of art,
+ *Return: 0 if the programming ran to completion
+ */
+int main(void)
+{
+	char varChr_cadena;
+	varChr_cadena="and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	fputs(varChr_cadena,stderr);
+
+	return (1);
+}
