855097678f39a27875dc45888978226a3f1e9db0
Ronald Rivero <ralexrivero@gmail.com>
2021-06-19 11:33:41 -0700

    low level programming in c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - print alphabet
+*
+ *Description: display alphabet
+ *Return: 0 if the programming ran to completion
+ */
+
+int main(void)
+{
+char i;
+/*char iHex;
+ */
+
+for (i = '0' ; i <= '9' ; i++)
+putchar(i);
+for (i = 0 ; i <= 5; i++)
+putchar('a'+i);
+putchar('\n');
+return (0);
+}
