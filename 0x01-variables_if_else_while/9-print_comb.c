4846992b0b1339b7c851a0564f968c6be0548400
Ronald Rivero <ralexrivero@gmail.com>
2021-06-19 11:46:54 -0700

    low level programming in c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - print alphabet
+*
+ *Description: display alphabet
+ *Return: 0 if the programming ran to completion
+ */
+
+int main(void)
+{
+int i;
+
+for (i = '0' ; i <= '9' ; i++)
+{
+putchar(i);
+putchar(' ');
+putchar(',');
+}
+putchar('\n');
+return (0);
+}
