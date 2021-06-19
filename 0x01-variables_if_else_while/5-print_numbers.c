84fd83b9f90afa7857fb6853773ca6cadc16cd13
Ronald Rivero <ralexrivero@gmail.com>
2021-06-18 19:51:28 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - print alphabet
+*
+ *Description: display alphabet
+ *Return: 0 if the programming ran to completion
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0 ; i <= 9 ; i++)
+putchar('\n');
+return (0);
+}
