a6e686c95c75de96a07b68c554b5ceed177e919c
Ronald Rivero <ralexrivero@gmail.com>
2021-06-20 08:44:36 -0700

    Low level programming in c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,16 @@ int main(void)
 {
 int i, j;
 
-for (i = '0'; i <= '9'; i ++)
-for (j = i + 1; j <= '9'; j ++)
+for (i = 0; i <= 9; i++)
+for (j = i + 1; j <= 9; j++)
 {
-putchar(i);
-putchar(j);
-putchar(',');
-putchar(' ');
+	putchar(i + '0');
+	putchar(j + '0');
+	if (i < 8)
+		{
+		putchar(',');
+		putchar(' ');
+		}		
 }
 putchar('\n');
 return (0);
