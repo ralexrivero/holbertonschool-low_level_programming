428d56a20db9b2a0ccc8b6241e1b3447c8d0d54a
Ronald Rivero <ralexrivero@gmail.com>
2021-06-22 16:57:44 -0700

    Functions nested loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,14 +2,19 @@
 
 /*
  * Prints "HOlberton" folowed by a new line
- *
+ * Main: _putchar prototype
  * Description: practicing functions an user libraries
  * Return: when succesfully execute the function
  */
 
 int main(void)
 {
-	_putchar(97);
+char char_str[9] = "Holberton";
+int i;
 
+for (i = 0; i = 9; i++)
+{
+	_putchar(char_str[i]);
+}
 	return (0);
 }
