747b0c994ee82c59ddc3f19614a1860f54d25802
Ronald Rivero <ralexrivero@gmail.com>
2021-06-22 16:15:28 -0700

    Functions nested loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,3 +1,15 @@
-#include <holberton.h>
+#include "holberton.h"
 
+/*
+ * Prints "HOlberton" folowed by a new line
+ *
+ * Description: practicing functions an user libraries
+ * Return: when succesfully execute the function
+ */
 
+int main(void)
+{
+	_putchar(97);
+
+	return (0);
+}
