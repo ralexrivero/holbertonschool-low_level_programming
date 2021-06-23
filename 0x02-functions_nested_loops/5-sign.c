21b8f649a9486d30a94324d2ad135e89cca6109f
Ronald Rivero <ralexrivero@gmail.com>
2021-06-23 08:24:21 -0700

    Function nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,11 +14,11 @@ if (n > 0)
 {
 	_putchar('+');
 	return (1);
-}else if (n == 0)
+} else if (n == 0)
 {
 	_putchar('0');
 	return (0);
-}else if (n < 0)
+} else if (n < 0)
 {
 	 _putchar('-');
 	 return (-1);
