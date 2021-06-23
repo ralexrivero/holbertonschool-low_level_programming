e6234f7a507c9bf7dfad28955663f0b922a055a7
Ronald Rivero <ralexrivero@gmail.com>
2021-06-22 19:40:39 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+*_islower check the lowercase
+*@c: the character to chek lowercase
+ *Description: _putchar prototype
+ *Return: - if the programming ran to completion
+ */
+
+int _islower(int c);
+{
+return(c >= 97 && <= 122);
+}
