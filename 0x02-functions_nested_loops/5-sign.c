ba19a54e6579d291ed5a3f1c951cbfe9ab16b9e3
Ronald Rivero <ralexrivero@gmail.com>
2021-06-22 19:59:29 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+*main - prints "Holberton"
+*
+ *Description: _putchar prototype
+ *Return: - if the programming ran to completion
+ */
+
+int main(void)
+{
+char char_str[9] = "Holberton";
+int i;
+
+for (i = 0; i <= 8; i++)
+{
+	_putchar(char_str[i]);
+}
+	_putchar('\n');
+
+	return (0);
+}
