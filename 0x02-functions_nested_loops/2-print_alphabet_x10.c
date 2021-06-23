654b554473cd34e2bf93ff1131c361c4458ce17a
Ronald Rivero <ralexrivero@gmail.com>
2021-06-22 18:47:04 -0700

    Functions nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-*print_alphabet_10 - prints the alphabet in lowercase
+*print_alphabet_x10 - prints the alphabet in lowercase
 *
  *Description: _putchar prototype
  *Return: - if the programming ran to completion
