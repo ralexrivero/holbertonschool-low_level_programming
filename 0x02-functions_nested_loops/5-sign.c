eec63f107970400c5ef33ccb361afc4d2807085c
Ronald Rivero <ralexrivero@gmail.com>
2021-06-23 08:30:28 -0700

    Functions nested tools

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,20 +7,19 @@
  * Return: 0 if 0, + if greate than zero, - if less than zero
  */
 
-
 int print_sign(int n)
 {
-if (n > 0)
-{
-	_putchar('+');
-	return (1);
-} else if (n == 0)
-{
-	_putchar('0');
-	return (0);
-} else if (n < 0)
-{
-	 _putchar('-');
-	 return (-1);
-}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+	return (-1);
 }
