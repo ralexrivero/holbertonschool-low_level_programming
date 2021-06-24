d01920928c7a0d2c5768780b1447edc83a239dd3
Ronald Rivero <ralexrivero@gmail.com>
2021-06-23 18:44:03 -0700

    Functions nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,15 +8,34 @@
 
 void times_table(void)
 {
-int i, j;
+	int i, j, k, l;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j + (i + 1);; i += 9)
+		for (j = 0; j < 10; j++)
 		{
-			_putchar (i);
-			_putchar (',');
-			_putchar (' ');
-			_putchar (' ');
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+			for (l = 0; l <= 1; l++)
+			{
+				_putchar(' ');
+			}
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
+}
