36102d3ceaecaadb155118e867637f1ff9ec88b5
Ronald Rivero <ralexrivero@gmail.com>
2021-06-24 11:42:53 -0700

    Debugging 0-main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - test function for prototype: positive_or_negative
+ * Description: debugging prototype
+ * Return: 0 if test is ok
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+
+	return(0);
+}
+
