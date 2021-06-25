7b0cec80079b0252db3a8cd4a0a99aab903b56ab
Ronald Rivero <ralexrivero@gmail.com>
2021-06-24 19:46:08 -0700

    More functions nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,8 +9,8 @@
 void more_numbers(void)
 {
 int i, j;
- 
-for (i = 0; i <= 10; i++)
+
+for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 14; j++)
 {
--- 0x04-more_functions_nested_loops/_5.c
+++ 0x04-more_functions_nested_loops/_5.c
@@ -10,7 +10,7 @@ void main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 	for (j = 0; j <= 14; j++)
 	{
