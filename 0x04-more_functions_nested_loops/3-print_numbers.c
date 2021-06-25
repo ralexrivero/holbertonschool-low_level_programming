786316ee9e3175626d4064df61f7e73ec4e90244
Ronald Rivero <ralexrivero@gmail.com>
2021-06-24 18:35:35 -0700

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print numbers
+ * Description: from 0 to 9
+ * Return: the numbers
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar (i);
+	}
+	_putchar ('\n');
+return (0);
+}
