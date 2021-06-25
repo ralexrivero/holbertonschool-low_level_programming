d06de009125c1cd3424c50fd750156e2563622f3
Ronald Rivero <ralexrivero@gmail.com>
2021-06-25 14:14:50 -0700

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_triangle
+ * Description: prints a triangle
+ * @size: the size, if greater than 0
+ * Return: when ran complete
+ */
+
+void print_triangle(int size)
+{
+int height, base, ind;
+
+if (size > 0)
+	for (height = 1; heigt <= size; height++)
+	{
+		for (ind = 1; ind <= (size - height); ind++)
+			_putchar(32);
+		for (base = 1; base <= height; base++)
+			_putchar(35);
+		_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/_10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints # to make triangles
+ * Description: prints a triangle
+ * @size: the size, if greater than 0
+ * Return: when ran complete
+ */
+
+void main(int size)
+{
+int height, base, ind;
+
+size = 10;
+if (size > 0)
+	for (height = 1; height <= size; height++)
+	{
+	for (ind = 1; ind <= (size - height); ind++)
+		putchar(32);
+	for (base = 1; base <= (height); base++)
+		putchar(35);
+		putchar('\n');
+}
+}
