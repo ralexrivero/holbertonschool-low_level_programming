b71a865de9c3a2856ddfef48af02cf7b951174f8
Ronald Rivero <ralexrivero@gmail.com>
2021-06-25 09:39:16 -0700

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz test
+ * Description: print numbers and text
+ * Return: zero when ran completely
+ */
+
+int main(void)
+{
+int c;
+
+for (c = 1; c <= 100; c++)
+{
+{
+	if ((c % 3) == 0 && (c % 5) == 0)
+	{
+		printf("FizzBuzz ");
+	}
+	else if ((c % 3) == 0)
+	{
+		printf("Fizz ");
+	}
+	else if ((c % 5) == 0)
+	{
+	printf("Buzz ");
+	}
+	else
+	{
+	printf("%i ", c);
+	}
+}
+}
+putchar('\n');
+}
