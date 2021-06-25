ab4a6882faa4dbb68abb05db4f563d77c98409ae
Ronald Rivero <ralexrivero@gmail.com>
2021-06-24 18:53:19 -0700

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print some digits
+ * Description: do not print 2 and 4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+int i;
+
+	for (i = 48; i <= 57; i++)
+		while (i != 50 || != 52)
+		{
+			_putchar (i);
+		}
+	_putchar ('\n');
+}
