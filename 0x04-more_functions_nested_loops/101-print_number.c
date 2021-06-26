7f552b3902ebaf2f3151e403c110519eb22573ba
Ronald Rivero <ralexrivero@gmail.com>
2021-06-25 19:45:24 -0700

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_number - print an integer
+ * Description: print an integer by prototype
+ * @n: integer to diplay
+ * Return: zero
+ */
+
+void print_number(int n)
+{
+unsigned int x;
+
+if (n < 0)
+{
+x = -n;
+_putchar('-');
+} else
+{
+x = n;
+}
+if (x / 10)
+{
+print_number(x / 10);
+}
+_putchar((x % 10) + '0');
+}
