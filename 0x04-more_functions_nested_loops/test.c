c063bb7aaade857a9ff825c0d9a9daacb6cfbbf3
Ronald Rivero <ralexrivero@gmail.com>
2021-06-26 10:10:15 -0700

    More functions nested loops

--- 0x04-more_functions_nested_loops/test.c
+++ 0x04-more_functions_nested_loops/test.c
@@ -1,51 +1,52 @@
-#include "holberton.h"
-
+#include <stdio.h>
 /**
- * print_number - print an integer
+ * main - print an integer
  * Description: print an integer by prototype
  * @n: integer to diplay
  * Return: zero
  */
-void print_number(int n)
+void main(int n)
 {
 	int a, b;
 
+	n = -765123;
 	if (n < 0)
 	{
 		a = -n;
-		_putchar('-');
+		putchar('-');
 	}
 	else
 		a = n;
 	b = a / 10000000000;
 	if (b)
-		_putchar(b + '0');
+		putchar(b + '0');
 	b = ((a / 1000000000) % 10);
 	if (b)
-		_putchar(b + '0');
+		putchar(b + '0');
 	b = ((a / 100000000) % 10);
 	if (b)
-		_putchar(b + '0');
+		putchar(b + '0');
 	b = ((a / 10000000) % 10);
 	if (b)
-		_putchar(b + '0');
+		putchar(b + '0');
 	b = ((a / 1000000) % 10);
 	if (b)
-		_putchar(b + '0');
+		putchar(b + '0');
 	b = ((a / 100000) % 10);
 	if (b)
-		_putchar(b + '0');
+		putchar(b + '0');
 	b = ((a / 10000) % 10);
 	if (b)
-		_putchar(b + '0');
+		putchar(b + '0');
 	b = ((a / 1000) % 10);
 	if (b)
-		_putchar(b + '0');
+		putchar(b + '0');
 	b = ((a / 100) % 10);
 	if (b)
-		_putchar(b + '0');
+		putchar(b + '0');
 	b = ((a / 10) % 10);
 	if (b)
-		_putchar(b + '0');
-	_putchar((a % 10) + '0');
+		putchar(b + '0');
+	putchar((a % 10) + '0');
+	putchar('\n');
 }
