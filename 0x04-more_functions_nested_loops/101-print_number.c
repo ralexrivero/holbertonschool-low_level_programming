37768a14dff4fe823bb59573f0dd698ba5c14593
Ronald Rivero <ralexrivero@gmail.com>
2021-06-27 21:04:46 -0700

    More functions nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,26 +9,25 @@ void print_number(int n)
 {
 int digits, n_d, i, power, n_obo;
 
-if (n < 0)
+if (n < 0) /* If n is smaller than 0, put a - sign and change number to positive */
 {
-_putchar('-');
-n = -n;
+	_putchar('-');
+	n = -n;
 }
-if (n == 0)
-{
-_putchar(n + '0');
-}
-digits = 0;
+digits = 0; /* count digits of n */
 n_d = n;
 while (n_d > 0)
 {
 n_d = n_d / 10;
 digits++;
 }
-while (digits > 0)
+if (digits == 0)
 {
+	digits = 1;
+}
+while(digits > 0){ /* power of ten according to number of digits*/
 power = 1;
-for (i = 1; i <= (digits -1); i++)
+for(i = 1; i <= (digits -1); i++)
 {
 power = (power * 10);
 }
