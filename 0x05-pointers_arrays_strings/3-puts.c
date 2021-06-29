8399b73315aba891b62a588792da74bb716c63ec
Ronald Rivero <ralexrivero@gmail.com>
2021-06-29 06:26:55 -0700

    Pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string
+ * Description: to the stdout
+ * @str: the string
+ * Return:
+ */
+
+void _puts(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+	_putchar (str[i]);
+}
+_putchar ('\n');
+}
