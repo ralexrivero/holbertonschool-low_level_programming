2c34d2c55674228edd077c97232b94724c6c6fe7
Ronald Rivero <ralexrivero@gmail.com>
2021-06-28 19:53:03 -0700

    Pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen - prototype function
+ * Description: return length of a string
+ * @s: the string
+ * Return: the value
+ */
+
+int _strlen(char *s)
+{
+printf("%lu", sizeof(*s));
+return (0);
+}
