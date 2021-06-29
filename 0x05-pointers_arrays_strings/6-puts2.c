391017c830026e1eeff66f4ee541b772d3e2dac1
Ronald Rivero <ralexrivero@gmail.com>
2021-06-29 12:07:01 -0700

    Pointers arrays strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,16 @@
 
 void puts2(char *str)
 {
-int i;
+	int len, i;
 
-for (i = 0; str[i] != '\0'; i += 2)
-{
-	_putchar(str[i]);
-}
-_putchar('\n');
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
