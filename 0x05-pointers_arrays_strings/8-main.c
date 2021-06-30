d2f432d861a8cdb3fa59559b94245464ced6932f
Ronald Rivero <ralexrivero@gmail.com>
2021-06-29 18:56:41 -0700

    Pointers arrays strings

--- 0x05-pointers_arrays_strings/8-main.c
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -7,13 +7,11 @@
  */
 int main(void)
 {
-    int array[5];
+	int array[1] = {
+		98
+	};
 
-    array[0] = 98;
-    array[1] = 402;
-    array[2] = -198;
-    array[3] = 298;
-    array[4] = -1024;
-    print_array(array, 5);
-    return (0);
+	print_array(array, 0);
+	print_array(array, -1);
+	return (0);
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,13 @@ void print_array(int *a, int n)
 {
 int i;
 if (n > 0)
+{
 for (i = 0; i < n - 1; i++)
 {
 	printf("%i, ", a[i]);
 }
 printf("%i\n", a[n - 1]);
+}
+printf("\n");
+return;
 }
