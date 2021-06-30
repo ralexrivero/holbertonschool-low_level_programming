4802f2c6f29d169d2867945768171cc58468f72c
Ronald Rivero <ralexrivero@gmail.com>
2021-06-29 19:09:21 -0700

    Pointers arrays strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,15 @@
 void print_array(int *a, int n)
 {
 int i;
+
 if (n > 0)
 {
 for (i = 0; i < n - 1; i++)
 {
 	printf("%i, ", a[i]);
 }
-printf("%i\n", a[n - 1]);
+printf("%i", a[n - 1]);
 }
+printf("\n");
 return;
 }
