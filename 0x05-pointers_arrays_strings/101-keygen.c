cf7e1c5e146d138f98c9fc89a5fe6323730f2d16
Ronald Rivero <ralexrivero@gmail.com>
2021-06-29 20:00:37 -0700

    Pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate random pass
+ * Description: passwords for the program
+ * Return: 0 when success
+ */
+int main(void)
+{
+	int psw[100], i, n, sum;
+
+	sum = 0;	
+	srand(time(NULL));
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (psw[i] + '0');
+		putchar(psw[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
