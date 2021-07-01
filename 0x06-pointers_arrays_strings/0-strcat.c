d1599fc3d40fdaa18266dc7a83e614644d46154c
Ronald Rivero <ralexrivero@gmail.com>
2021-07-01 07:21:53 -0700

    Pointers arrays strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenate strings
+ * Description: two strings
+ * @dest: final string
+ * @src: initial string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i = 0, j = 0;
+
+while (dest[i] != '\0')
+	i++;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+return(dest);
+}
