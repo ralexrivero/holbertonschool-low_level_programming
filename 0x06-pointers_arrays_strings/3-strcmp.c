a7d8f471aafc3d0dfa875851fdbab0bee29af86a
root <ralexrivero@gmail.com>
2021-07-01 16:51:58 -0700

    Pointers arrays strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,13 +2,22 @@
 
 /**
  * _strcmp - works like strcmp
- * Description: 
- * Return: 
- * @s1:
- * @s2:
+ * Description: compares two strings
+ * Return: if equal or 1 if are different
+ * @s1: first string
+ * @s2: second string
  */
 
 int _strcmp(char *s1, char *s2)
 {
-
+while (*s1 != '\0')
+{
+if (*s1 != *s2)
+{
+return (*s1 - *s2);
+}
+s1++;
+s2++;
+}
+return (0);
 }
