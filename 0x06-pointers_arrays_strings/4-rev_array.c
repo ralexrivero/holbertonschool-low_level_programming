66b7922fefd75a63a000db8c4d6ae73b5ef73e6b
root <ralexrivero@gmail.com>
2021-07-01 16:57:25 -0700

    Pointers arrays strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse array
+ * Description: of integers
+ * Return: reversed array
+ * @a: array
+ * @n: number of elements
+ */
+
+void reverse_array(int *a, int n)
+{
+
+}
