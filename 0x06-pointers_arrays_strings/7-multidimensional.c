2b8fb1e1f3d2f3ddf245355da12c7eedddb7b82e
root <ralexrivero@gmail.com>
2021-07-02 06:36:54 -0700

    Pointers arrays strings

--- 0x06-pointers_arrays_strings/7-multidimensional.c
+++ 0x06-pointers_arrays_strings/7-multidimensional.c
@@ -2,25 +2,24 @@
 
 /**
  * leet - encodes a string into 1337
+ * @s: the string to change and encode
  * Descripion: encodes usin the leet
  * Return: when done complete
- * @str: string to encode
+ * string to encode
  */
 
 char *leet(char *s)
 {
-char *lt[5][3] = {{'a', 'A', '4'}, {'e', 'E', '3'}, {'o', 'O', '0'},
+char lt[][3] = {{'a', 'A', '4'}, {'e', 'E', '3'}, {'o', 'O', '0'},
 {'t', 'T', '7'}, {'l', 'L', '1'}};
 int i, j;
 
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; j < 4; j++)
+for (j = 0; j <= 4; j++)
 {
-	if (s[i] == lt[j][0] && lt[j][1])
-	{
-	s[i] = lt[j][2];
-	}
+if (s[i] == lt[j][0] /* && lt[j][1]*/)
+s[i] = lt[j][2];
 }
 }
 return (s);
