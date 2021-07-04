c69e659290a85df3f1647ea76849e954d9dfb7d8
root <ralexrivero@gmail.com>
2021-07-04 10:03:31 -0700

    Pointers arrays strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,7 +2,7 @@
 
 /**
  * infinite_add - adds two numbers
- * Description: 
+ * Description: adds two numbers asummig always numbers
  * @n1: one number
  * @n2: other number
  * @r: buffer to store the result
@@ -23,31 +23,31 @@ l = i;
 else
 l = j;
 if (l + 1 > size_r)
-		return (0);
-	r[l] = '\0';
-	for (k = l - 1 ; k >= 0 ; k--)
-	{
-		i--;
-		j--;
-		if (i >= 0)
-			f = n1[i] - '0';
-		else
-			f = 0;
-		if (j >= 0)
-			s = n2[j] - '0';
-		else
-			s = 0;
-		r[k] = (f + s + d) % 10 + '0';
-		d = (f + s + d) / 10;
-	}
-	if (d == 1)
-	{
-		r[l + 1] = '\0';
-		if (l + 2 > size_r)
-			return (0);
-		while (l-- >= 0)
-			r[l + 1] = r[l];
-		r[0] = d + '0';
-	}
+return (0);
+r[l] = '\0';
+for (k = l - 1 ; k >= 0 ; k--)
+{
+i--;
+j--;
+if (i >= 0)
+f = n1[i] - '0';
+else
+f = 0;
+if (j >= 0)
+s = n2[j] - '0';
+else
+s = 0;
+r[k] = (f + s + d) % 10 + '0';
+d = (f + s + d) / 10;
+}
+if (d == 1)
+{
+r[l + 1] = '\0';
+if (l + 2 > size_r)
+return (0);
+while (l-- >= 0)
+r[l + 1] = r[l];
+r[0] = d + '0';
+}
 return (r);
 }
