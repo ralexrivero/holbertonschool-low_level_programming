ce8b538b266a6d10fcff725694a16b682538cec2
root <ralexrivero@gmail.com>
2021-07-05 07:00:45 -0700

    Pointers arrays strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,13 +3,20 @@
 /**
  * _memset - fills memory
  * Description: with constant byte
- * @s: char pointer
- * @b: char
- * @n: unsigned int
+ * @s: memory to fill
+ * @b: char to be copied
+ * @n: times to copy b
  * Return: when done
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
+unsigned int i = 0;
 
+while (i < n)
+{
+s[i] = b;
+i++;
+}
+return (s);
 }
