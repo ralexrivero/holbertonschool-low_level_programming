be50c924b89f17a94249939daa0b362babe011db
root <ralexrivero@gmail.com>
2021-07-04 22:04:49 -0700

    Pointers arrays strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memory
+ * Description: with constant byte
+ * @s: char pointer
+ * @b: char
+ * @n: unsigned int
+ * Return: when done
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
