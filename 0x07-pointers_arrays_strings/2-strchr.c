fc41ad60e86c43d24cf005a7019594700e1a097b
root <ralexrivero@gmail.com>
2021-07-05 12:32:53 -0700

    Pointers arrays strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strch - locates a character in a string
+ * Description: the first occurrence of the character
+ * Return: a pointer to the match or NULL if not found
+ * @s: string to be searched
+ * @c: character to search
+ */
+
+char *_strchr(char *s, char c)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+	if (s[i] == c)
+	{
+		return (s + i);
+	}
+	i++;
+}
+return (NULL);
+}
