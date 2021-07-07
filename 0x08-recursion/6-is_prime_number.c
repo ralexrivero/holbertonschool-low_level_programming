732c7262441a8bcf6811599678655ce177662a1d
root <ralexrivero@gmail.com>
2021-07-07 10:09:20 -0700

    Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - evaluate a number
+ * Description: if is prime number
+ * Return: 1 if primer, otherwise 0
+ * @n: the number
+ */
+
+int is_prime_number(int n)
+{
+if (n <= 1)
+	return (0);
+return (_prime(n, n - 1));
+}
+
+/**
+ * _prime(int n, int i)
+ * Description: recusive prime calculation
+ * Return: when prime 1 otherwise 0
+ * @n: the number
+ * @i: the iterator
+ */
+
+int _prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (i > 0 && n % i == 0)
+		return (0);
+	return (_prime(n, i - 1));
+}
