bcd55b231ce4cba8c6719dbf35ea9a01e6194da9
root <ralexrivero@gmail.com>
2021-07-07 07:15:40 -0700

    Recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,11 +4,12 @@
  * _strlen_recursion - return length
  * Description: of a string
  * @s: the string to evaluate
+ * Return: the addition of one byte of length
  */
 
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 return (0);
-return 1 + _strlen_recursion(s + 1);
+return (1 + _strlen_recursion(s + 1));
 }
