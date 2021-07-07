d64e36c0479068a15c31baafc6c8af2cc264ad1e
root <ralexrivero@gmail.com>
2021-07-07 09:18:02 -0700

    Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - square root
+ * Description: only natural numbers
+ * @n: the number to evaluate
+ * Return: -1 if not natual
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+	return(-1);
+return( _sqrt_recursion_i(n, 0));
+}
+
+/**
+ * _sqrt_recursion_i - square for natural
+ * Description: recursion for square
+ * @n: number
+ * @i: iterator
+ * Return: the natural square
+ */
+int _sqrt_recursion_i(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return(_sqrt_recursion_i(n, i + 1));
+}
