7e9b53645572f8c96f9b3bc252ec576feefe60d2
root <ralexrivero@gmail.com>
2021-07-07 07:13:04 -0700

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - return length
+ * Description: of a string
+ * @s: the string to evaluate
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+return 1 + _strlen_recursion(s + 1);
+}
