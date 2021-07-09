5350846139b00161309200ca60f191f1ee503f6a
root <ralexrivero@gmail.com>
2021-07-09 16:26:50 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,12 @@
  * Description: if rename the program, it will print the name
  * without compiling
  * Return: the name, EXIT_SUCCESS
+ * @argc: count
+ * @argv: string
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s,\n", argv[0]);
-exit (EXIT_SUCCESS);
+	printf("%s\n", argv[0]);
+exit(EXIT_SUCCESS);
 }
