10023dc328d7c17587dce66192f60461c1691dce
root <ralexrivero@gmail.com>
2021-07-09 06:16:50 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- *
- * Description:
- * Return:
+ * main - print the name of program
+ * Description: if rename the program, it will print the name
+ * without compiling
+ * Return: the name, EXIT_SUCCESS
  */
 
-
+int main(int argc, char *argv[])
+{
+	printf("%s,\n", argv[0]);
+exit (EXIT_SUCCESS);
+}
