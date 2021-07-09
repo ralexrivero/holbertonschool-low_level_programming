d4367573ff6a7ed5c91a706abc87ffe68f26dfec
root <ralexrivero@gmail.com>
2021-07-09 07:37:15 -0700

    argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,9 +1,21 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- *
- * Description:
- * Return:
+ * main - print the result of the multiplication of two numbers
+ * Description: assume that the two numbers and the result
+ * cand be stored in int
+ * Return: 1 and pritn Error if not recive two arguments
  */
 
-
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1])* atoi(argv[2]));
+	exit (EXIT_SUCCESS);
+}
