cbf31bae326faa34f8753c0553b043e9ffe08a7b
root <ralexrivero@gmail.com>
2021-07-09 18:32:28 -0700

    argc argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,11 +6,13 @@
  * main - prints the number of arguments passed into in
  * Description: print a number, followed by a new line
  * Return: EXIT_SUCCESS
+ * @argc: count arguments
+ * @argv: argument vector
  */
 
 int main(int argc, char *argv[])
 {
 	(void)argv;
-	printf("%d\n", argc -1);
+	printf("%d\n", argc - 1);
 	exit (EXIT_SUCCESS);
 }
