82f1275cee222854695a63127edcc7f9c302257a
root <ralexrivero@gmail.com>
2021-07-09 17:10:00 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,12 +7,15 @@
  * Description: if rename the program, it will print the name
  * without compiling
  * Return: the name, EXIT_SUCCESS
- * @argc: count
- * @argv: string
+ * @argc: argument count
+ * @argv: argument vector
  */
 
 int main(int argc, char *argv[])
 {
+if (argc > 0)
+{
 	printf("%s\n", argv[0]);
+}
 exit(EXIT_SUCCESS);
 }
