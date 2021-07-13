67b37315bdf440b33859aafb77b8741856844282
root <ralexrivero@gmail.com>
2021-07-13 08:33:02 -0700

    malloc free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * create_array - create array of chars
+ * Description: initialize with a specific char
+ * @size: the size of char
+ * @c: the c
+ * Return: pointer of the array, NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+}
+
