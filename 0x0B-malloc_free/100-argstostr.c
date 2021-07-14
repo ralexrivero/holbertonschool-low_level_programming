59853d4b1a0fd9389e1566a1718157b0558f64f8
root <ralexrivero@gmail.com>
2021-07-13 17:19:28 -0700

    malloc free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,33 +13,24 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
-	char *str;
+int i, j, k = 0, l = 0;
+char *str;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
-	}
-	str = malloc(sizeof(char) * (len + 1));
-
-	if (str == NULL)
-		return (NULL);
-
-	k = 0;
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-		str[k] = '\n';
-		k++;
-	}
-	return (str);
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+for (j = 0; av[i][j]; j++)
+l++;
+l += ac + 1;
+str = malloc(sizeof(char) * l);
+if (!str)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+str[k++] = av[i][j];
+str[k++] = '\n';
+}
+str[k] = '\0';
+return (str);
 }
