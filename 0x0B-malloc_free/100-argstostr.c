8b16557f280bdb09376334aece23be2fe1339663
root <ralexrivero@gmail.com>
2021-07-13 17:39:54 -0700

    malloc free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  **argstostr - concatenates all the arguments of your program
@@ -9,31 +10,40 @@
  *
  * Return:  a pointer to a new string, or NULL if it fails
  */
-
 char *argstostr(int ac, char **av)
 {
-	char *s;
-	int sum;
-	int i, j, k;
+	int i = 0, j, length = 0, l, k;
+	char *string;
 
-	sum = 0;
-	if (ac == 0 || av == NULL)
+	if (ac == 0 || av == 0)
+	return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			j++;
+		}
+		length  +=  j + 1;
+	}
+	string = malloc(sizeof(char) * (length + 1));
+
+	if (string == NULL)
+	{
 		return (NULL);
-	for (i = 0; av[i] != NULL; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			sum += 1;
-		sum += 1;
 	}
-	sum += 1;
-	s = malloc(sum * sizeof(char));
-	k = 0;
-	for (i = 0; av[i] != NULL; i++)
+	length = 0;
+	for (k = 0; k < ac; k++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++, k++)
-			s[k] = av[i][j];
-		s[k] = '\n';
-		k++;
+		for (l = 0; av[k][l] != '\0'; l++)
+		{
+			*(string + length) = av[k][l];
+			length++;
+		}
+		*(string + length) = '\n';
+		length++;
 	}
-	return (s);
+
+	return(string);
 }
