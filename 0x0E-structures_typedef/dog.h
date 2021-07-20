6ba7afb9ab3c4a4df4f7e8ba5154bdde6e733635
root <ralexrivero@gmail.com>
2021-07-19 19:01:05 -0700

    structures typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,6 +9,11 @@
  * Description: structure basic for dog's info
  */
 
-typedef struct dog { char *name; float age; char *owner; } dog;
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
 
 #endif
