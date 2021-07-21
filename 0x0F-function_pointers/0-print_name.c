d8fe84ed4e13e080c17233ccd314bea24d8ed8a2
root <ralexrivero@gmail.com>
2021-07-21 16:41:05 -0700

    Function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,15 +5,16 @@
 /**
  * print_name - prints a name
  * @name: string of name
+ * @f: function pointer
  * Description: entry point to function
  * Return: 0 if name or f is null, otherwise pointer to function
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-    if(name == NULL || f == NULL)
-    {
-    return;        
-    }
-    f(name);
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name);
 }
