edaa34b38895bd8fc3f19a218d12f355f337587f
root <ralexrivero@gmail.com>
2021-07-22 09:30:35 -0700

    variadic function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+
+/**
+ * print_numbers - print number, followed by a line
+ * @separator: tokenize the string
+ * @n: number of arguments pased to the function
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list list;
+
+va_start(list, n);
+
+for (i = 0; i < n; i++)
+{
+	if (!separator)
+		printf("%d", va_arg(list, int));
+	else if (separator && i == 0)
+		printf("%d", va_arg(list, int));
+	else
+		printf("%s%d", separator, va_arg(list, int));
+}
+va_end(list);
+printf("\n");
+}
