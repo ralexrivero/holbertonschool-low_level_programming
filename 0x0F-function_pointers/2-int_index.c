5f2e80aaa9e4d08f515958ca11a11051ca3a72c2
root <ralexrivero@gmail.com>
2021-07-21 19:50:07 -0700

    function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,18 +7,18 @@
  * @cmp: pointer to the function to compare
  * Return: index of the first element
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+int i;
 
-	if (array && cmp)
-	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-		}
-	}
-
-	return (-1);
+if (array && cmp)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+}
+return (-1);
 }
