4e16d01e053139cca9849f946a1a41416d0e984e
root <ralexrivero@gmail.com>
2021-07-22 10:18:30 -0700

    variadic function

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - print anyting
+ * @format: list of types of arguments
+ * Description: lika printf
+ * Return: the output formated in char, integer, float or char*
+ * string NULL print (nil)
+ */
+
+
