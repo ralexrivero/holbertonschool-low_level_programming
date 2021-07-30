330ae5dbdf60d5dd84f5e74eed267478d35aa340
root <ralexrivero@gmail.com>
2021-07-29 20:20:14 -0700

    betty on lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -12,9 +12,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 /* prototypes */
