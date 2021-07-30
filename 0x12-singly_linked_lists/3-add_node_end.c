8a94134aec8e21c12f3c6e02e093380e40b883f8
root <ralexrivero@gmail.com>
2021-07-29 20:01:22 -0700

    linked list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,20 @@
+#include <lists.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to list
+ * Return: nomber of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		(!h->str) ? printf("[0] (nil)\n") :
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		size++;
+	}
+	return (size);
+}
