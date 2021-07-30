f8aca0d9ac0a3ab333c5f7e38309a6d0fae1fbc2
root <ralexrivero@gmail.com>
2021-07-30 16:06:33 -0700

    printf before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,20 +1,11 @@
-#include <lists.h>
+#include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to list
- * Return: nomber of nodes
+ * hare_tortoise - prints text before execute main
  */
-size_t print_list(const list_t *h)
-{
-	size_t size = 0;
 
-	while (h)
-	{
-		(!h->str) ? printf("[0] (nil)\n") :
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		size++;
-	}
-	return (size);
+void __attribute__((constructor)) hare_tortoise()
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
