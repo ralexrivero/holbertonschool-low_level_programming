a32d80d389ad0ea21cad9ddc3959f13c62329401
root <ralexrivero@gmail.com>
2021-07-30 15:18:04 -0700

    count elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,18 @@
-#include <lists.h>
+#include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
+ * list_len - count the elements of a list_t list
  * @h: pointer to list
- * Return: nomber of nodes
+ * Return: number of elments
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
-	size_t size = 0;
+	size_t elements = 0;
 
 	while (h)
 	{
-		(!h->str) ? printf("[0] (nil)\n") :
-			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		size++;
+		elements++;
 	}
-	return (size);
+	return (elements);
 }
