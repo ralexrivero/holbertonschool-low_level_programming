1a4f383c40e64a5a570ed421826c9c5b8e307fac
root <ralexrivero@gmail.com>
2021-08-02 12:11:02 -0700

    listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+
+	return ();
+}
