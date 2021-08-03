ea28bee87abe4d440eedfa1a4c84a777af0cfe06
root <ralexrivero@gmail.com>
2021-08-03 13:28:47 -0700

    betty style fix

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,8 +3,8 @@
 /**
  * add_nodeint - add node at list
  * Return: address of new element of NULL if fail
- * @head:
- * n: 
+ * @head: first node in list
+ * n: data to add
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
@@ -12,7 +12,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *add_node;
 
 	add_node = malloc(sizeof(listint_t));
-	if(!add_node)
+	if (!add_node)
 		return (NULL);
 	add_node->n = n;
 	add_node->next = *head;
