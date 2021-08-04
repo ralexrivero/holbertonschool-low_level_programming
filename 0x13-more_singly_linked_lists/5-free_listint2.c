27f9920ae1ad68b781af541fb954d6f4f7186074
root <ralexrivero@gmail.com>
2021-08-03 19:54:42 -0700

    free again

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list
+ * @head: pointer to head
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *buffer;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		buffer = (*head)->next;
+		free(*head);
+		*head = buffer;
+	}
+	*head = NULL;
+}
