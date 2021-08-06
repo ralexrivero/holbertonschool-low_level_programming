14ed341ab4579ae15536a49d5bb80ca2706ac6f8
ralexrivero <ralexrivero@gmail.com>
2021-08-05 22:59:58 -0400

    fix betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int i;
-	unsigned int varUInt_uint = 0;
+int i;
+unsigned int varUInt_uint = 0;
 
-	if (!b)
-		return (0);
-
-    for (i = 0; b[i]; i++)
-    {
-        if (b[i] < '0' || b[i] > '1')
-        return (0);
-        varUInt_uint = 2 * varUInt_uint + (b[i] - '0');
-    }
-    return (varUInt_uint);
+if (!b)
+return (0);
+for (i = 0; b[i]; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+varUInt_uint = 2 * varUInt_uint + (b[i] - '0');
+}
+return (varUInt_uint);
 }
