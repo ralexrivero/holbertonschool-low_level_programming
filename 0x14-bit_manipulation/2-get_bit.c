a3b728cc313c5edcdd42cae22ca519cd281ac5dc
ralexrivero <ralexrivero@gmail.com>
2021-08-06 19:46:49 -0400

    Get bit sizeof

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (!n && !index)
+    if (index > (sizeof(unsigned long int) * 8))
     return (-1);
     return (n >> index) & 1;
 }
