904fc0abda90636e41661419ccc8fc6f6d9c34f5
ralexrivero <ralexrivero@gmail.com>
2021-08-05 21:37:46 -0400

    initial files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * binary_to_uint - binary to unsigned int
+ * @b: pointer to string of 0 y 1
+ * Return: converted number
+ * 0 if there are other chars than 0 y 1
+ * 0 if b is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+    
+}
