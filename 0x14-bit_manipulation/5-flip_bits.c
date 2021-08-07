29f32d2edfa394448cf228848f11705dd50804a0
ralexrivero <ralexrivero@gmail.com>
2021-08-06 20:53:36 -0400

    betty style

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
-* flip_bits - c number of bits needed to flip to get from one number to another
-* @n: number to be fliped
+* flip_bits - c number of bits needed to flip to get from one number to other
+* @n: number to be fliped to
 * @m: number to be fliped to
 * Return: number of bits needed to flip
 **/
@@ -14,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	varUIn_qnt = 0;
 	for (varUIn_num = n ^ m; varUIn_num != 0; varUIn_num = varUIn_num >> 1)
 	{
-		varUIn_qnt += (varUIn_num & 1);
+	varUIn_qnt += (varUIn_num & 1);
 	}
 	return (varUIn_qnt);
 }
