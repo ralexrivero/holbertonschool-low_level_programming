c40cb62d221fa3c9713282bd9778cdcca7d9ae7c
ralexrivero <ralexrivero@gmail.com>
2021-08-09 20:14:12 -0400

    read textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+  #include <main.h>
+  /**
+  * read_textfile - reads a text file and prints in POSIX stdout
+  * @filename: file to read
+  * @letters: number of letter it should read and print
+  * Return: actual number of letter it could read and print
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int varInt_file, varInt_count, varInt_stats;
+	char arrChr_buff[BUFFERSIZE];
+
+	if (filename == NULL)
+		return (0);
+	varInt_file = open(filename, O_RDONLY);
+	if (varInt_file == -1)
+		return (0);
+	varInt_count = 0;
+	varInt_stats = 1;
+	while (letters > BUFFERSIZE && varInt_stats != 0)
+	{
+		varInt_stats = read(varInt_file, arrChr_buff, BUFFERSIZE);
+		write(STDOUT_FILENO, arrChr_buff, varInt_stats);
+		varInt_count += varInt_stats;
+		letters -= BUFFERSIZE;
+	}
+	varInt_stats = read(varInt_file, arrChr_buff, letters);
+	write(STDOUT_FILENO, arrChr_buff, varInt_stats);
+	varInt_count += varInt_stats;
+	close(varInt_file);
+	return (varInt_count);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define BUFFERSIZE 1204
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
