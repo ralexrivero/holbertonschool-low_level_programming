c09641cac41c5d936a0e07899419bd2eb38a3c67
ralexrivero <ralexrivero@gmail.com>
2021-09-02 17:06:46 -0400

    very long multiply

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,6 +3,19 @@
 #include "holberton.h"
 
 /**
+ * _strlen - string lenght
+ * @str: pointer to character to be measured
+ * Return: the string lenght
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+		len++;
+	return (len);
+}
+/**
  * main - multiplies two positive numbers
  * @argc: n arguments
  * @argv: args
@@ -10,24 +23,48 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned long m;
-	int i, j;
+	int mul = 0, i = 0, j = 0, k = 0, m = 0, len1 = 0, len2 = 0, temp = 0;
+	int *prod;
 
+/* control only two arguments passed */
 	if (argc != 3)
 	{ printf("Error\n");
 		exit(98); }
+/* check if the characters are all numbers */
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-				exit(98); }
+			printf("Error\n"), exit(98);
 		}
 
 	}
-	m = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", m);
+/* obtain lenght of the two strings */
+len1 = _strlen(argv[1]);
+len2 = _strlen(argv[2]);
+/* allocate memory for product */
+prod = calloc((len1 + len2), sizeof(int *));
+if (prod == NULL)
+printf("Error\n"), exit(98);
+
+/* multiply arrays */
+	for (j = len2 - 1; j >= 0; j--)
+	{
+		for (k = len1 -1; k >= 0; k--)
+		{
+			mul = (argv[2][j] - '0') * (argv[1][k] - '0') + prod[j + k + 1];
+			prod[j + k + 1] = mul % 10;
+			prod[j + k] += mul / 10;
+		}
+	}
+/* print array */
+for (i = 0; i <= (len1 + len2 - 1); i++)
+{
+	_putchar(prod[i] + '0');
+}
+	_putchar('\n');
+	free(prod);
 	exit(EXIT_SUCCESS);
 	return (0);
 }
