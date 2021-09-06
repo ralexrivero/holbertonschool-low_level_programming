0bd0585e4b4f3d4d9268d2e27de091c9a5cadfd5
ralexrivero <ralexrivero@gmail.com>
2021-09-06 18:38:30 -0400

    restart linked list projects

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a linked list using 1 loop
+ * @head: pointer to the head of the list
+ * Return: the head of the reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+        
+}
