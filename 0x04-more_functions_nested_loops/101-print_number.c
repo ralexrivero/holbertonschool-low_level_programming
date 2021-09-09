7d7e813007acd65ed6bc4a218879dfa535821ffa
ralexrivero <ralexrivero@gmail.com>
2021-09-08 22:46:39 -0400

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,33 +7,34 @@
  */
 void print_number(int n)
 {
-unsigned int digits, n_d, i, power, n_obo;
+	unsigned int x, i = 1, y;
 
-if (n < 0) /* Put a - sign and change number to positive */
-{
+	if (n < 0)
+	{
 	_putchar('-');
-	n = -n;
-}
-digits = 0; /* count digits of n */
-n_d = n;
-while (n_d > 0)
-{
-n_d = n_d / 10;
-digits++;
-}
-if (digits == 0)
-{
-	digits = 1;
-}
-while (digits > 0)
-{ /* power of ten according to number of digits*/
-power = 1;
-for (i = 1; i <= (digits - 1); i++)
-{
-power = (power * 10);
-}
---digits;
-n_obo = (n / power) % 10;
-_putchar(n_obo + '0');
-}
+	y = -n;
+	x = y;
+	}
+	else
+	{
+	y = n;
+	x = y;
+	}
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		while (x > 9)
+		{
+			i *= 10;
+			x /= 10;
+		}
+		while (i > 1)
+		{
+			_putchar((y / i) + '0');
+			y %= i;
+			i /= 10;
+		}
+		_putchar(y + '0');
+	}
 }
