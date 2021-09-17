f0f9ae0ade3e0892adf445ebc54d9d9ad07fa352
ralexrivero <ralexrivero@gmail.com>
2021-09-16 22:13:19 -0400

    fix border

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,11 +8,13 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
+	if (!h)
+		return (0);
 	while (h)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		nodes++;
 	}
 	return (nodes);
 }
