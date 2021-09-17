e67f170c1c33c8e5f9e643efc29165cfbc230dd0
ralexrivero <ralexrivero@gmail.com>
2021-09-16 21:56:43 -0400

    fix header

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -9,13 +9,15 @@
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
+ *
  * Description: doubly linked list node structure
+ * 
  */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
