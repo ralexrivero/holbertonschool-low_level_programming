05ef92daa7b1fb8393557ff10520694ca0bf4af5
ralexrivero <ralexrivero@gmail.com>
2021-09-21 18:08:48 -0400

    add .c files for lib

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *_islower - check the lowercase
+ *@c: the character to chek lowercase
+ *Description: _putchar prototype
+ *Return: - if is lowercase 1, 0 other values
+ */
+
+int _islower(int c)
+{
+return (c >= 97 && c <= 122);
+}
---
+++ 0x18-dynamic_libraries/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - display a text
+*
+ *Description: display "Programming is like
+ *building a multilingual puzzle using the puts function.
+ *Return: 0 when finish
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
