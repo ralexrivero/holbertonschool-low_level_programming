ab2500d637614b41df434bb330ead7c8c0b1edf3
ralexrivero <ralexrivero@gmail.com>
2021-10-08 22:18:44 -0300

    fix comment

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,8 +1,9 @@
 #include "hash_tables.h"
 /**
- * hash_djb2 - ab cd
- * @str: ab cd
- * Return:  asdf d
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
