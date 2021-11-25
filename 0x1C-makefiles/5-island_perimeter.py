5caac330ed9b4bef2e5d53c8838e4be4fe4124af
ralexrivero <ralexrivero@gmail.com>
2021-11-25 18:59:19 -0300

    change file permission

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,7 +6,7 @@ returns the perimeter of the island described in grid
 
 def island_perimeter(grid):
     """
-    returns the perimeter of the island described in grid
+    count adyacent free sides
     """
     count = 0
     if grid is None:
