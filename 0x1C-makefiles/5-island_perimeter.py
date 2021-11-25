29e6c94c326b758d73193f044f6cbd18b9864e59
ralexrivero <ralexrivero@gmail.com>
2021-11-25 19:24:31 -0300

    fix condition

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,15 +15,15 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 # up
-                if grid[i - 1][j] == 0 or not grid[i][j]:
+                if grid[i - 1][j] == 0 or grid[i - 1][j] is None:
                     count += 1
                 # right
-                if grid[i][j + 1] == 0 or not grid[i][j]:
+                if grid[i][j + 1] == 0 or grid[i][j + 1] is None:
                     count += 1
                 # bottom
-                if grid[i + 1][j] == 0 or not grid[i][j]:
+                if grid[i + 1][j] == 0 or grid[i + 1][j] is None:
                     count += 1
                 # left
-                if grid[i][j - 1] == 0 or not grid[i][j]:
+                if grid[i][j - 1] == 0 or grid[i][j - 1] is None:
                     count += 1
     return(count)
