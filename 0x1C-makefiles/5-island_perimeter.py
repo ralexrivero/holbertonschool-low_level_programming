ac59b93460c6c081047cc065efbf94605142e0a5
ralexrivero <ralexrivero@gmail.com>
2021-11-25 19:20:15 -0300

    change is None for not

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,15 +15,15 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 # up
-                if grid[i - 1][j] == 0 or grid[i][j] is None:
+                if grid[i - 1][j] == 0 or not grid[i][j]:
                     count += 1
                 # right
-                if grid[i][j + 1] == 0 or grid[i][j] is None:
+                if grid[i][j + 1] == 0 or not grid[i][j]:
                     count += 1
                 # bottom
-                if grid[i + 1][j] == 0 or grid[i][j] is None:
+                if grid[i + 1][j] == 0 or not grid[i][j]:
                     count += 1
                 # left
-                if grid[i][j - 1] == 0 or grid[i][j] is None:
+                if grid[i][j - 1] == 0 or not grid[i][j]:
                     count += 1
     return(count)
