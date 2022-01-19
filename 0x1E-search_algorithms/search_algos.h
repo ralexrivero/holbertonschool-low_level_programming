5b00b6854c405824f3c63a880fb67a347423b94c
ralexrivero <ralexrivero@gmail.com>
2022-01-19 02:06:17 -0300

    prototypes to header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -37,5 +37,15 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 
+/* prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
 
 #endif /* Search algos */
